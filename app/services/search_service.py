a870491f4b8466b4749966ab8e2a3fd15dbc51ad
Huy <hoangg.huyy2003@gmail.com>
2025-10-09 00:11:35 +0700

    ...

--- app/services/search_service.py
+++ app/services/search_service.py
@@ -1,14 +1,15 @@
 from qdrant_client import QdrantClient
 from qdrant_client.http import models
 from app.services.embed_service import embedding_query
+from app.config import get_settings
+settings = get_settings()
+Collection_Name = settings.COLLECTION_NAME
 
-COLLECTION_NAME = "datachatbot"
-
-client = QdrantClient(url="http://localhost:6333")
+client = QdrantClient(host=settings.QDRANT_HOST, port=settings.QDRANT_PORT)
 def search_qdrant(query:str, top_k: int =3):
     query_vector = embedding_query(query)
     hits= client.query_points(
-        collection_name=COLLECTION_NAME,
+        collection_name=Collection_Name,
         query=query_vector,
         limit=top_k,
     )
